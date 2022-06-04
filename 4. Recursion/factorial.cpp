f743bd7faf4c80cf9e890ceb65f813944905117c
vishesh dugar <dugarvishesh@gmail.com>
2022-06-04 11:57:19 +0530

    feat: config .gitignore

--- 4. Recursion/factorial.cpp
+++ 4. Recursion/factorial.cpp
@@ -4,4 +4,18 @@ using namespace std;
 int main(){
     int fact(int n);
     int n = 8;
+    int ans = fact(n);
+    cout << ans << endl;
 }
+
+int fact(int n){
+    if (n == 1){
+        return 1;
+    }
+    else {
+        return n * fact(n-1);
+    }
+}
+
+// is this tail-recursive ? NO, as the compiler still needs to 
+// remember the output of previous calls while calculating result
---
+++ 4. Recursion/tr_fact.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int fact(int n, int k);
+    int n = 6; int k = 1;
+    int ans = fact(n,k);
+    cout << ans << endl;
+}
+
+int fact(int n, int k){
+    if (n==1){
+        return k;
+    }
+    else {
+        return fact(n-1, k * n);
+    }
+}
+
+// is this tail recursive? Yes, as the compiler does not
+// need to remember the previous outocomes.
+// in this way, tail recursion algos are faster than recursion algo
+// as the entire function call overhead is ignored.
