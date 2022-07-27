11b64f548ce6d271c920bc9121029b1e3d04a241
vishesh dugar <dugarvishesh@gmail.com>
2022-07-27 11:13:15 +0530

    feat: common tree data changes

--- 16. BST/first.cpp
+++ 16. BST/first.cpp
@@ -1,14 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node{
-    Node* left;
-    Node* right;
+struct Node
+{
+    Node *left;
+    Node *right;
     int key;
 
-    Node(int k){
+    Node(int k)
+    {
         key = k;
         left = right = NULL;
     }
 };
 
+int main()
+{
+    Node *root = new Node(30);
+    Node *Lchild = new Node(20);
+    Node *Rchild = new Node(40);
+    Node *LLchild = new Node(10);
+    Node *LRchild = new Node(25);
+    root->left = Lchild;
+    root->right = Rchild;
+    Lchild->left = LLchild;
+    Lchild->right = LRchild;
+}
